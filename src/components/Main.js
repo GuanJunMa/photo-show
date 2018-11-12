669ab539ae7525273b486c5d9ca615ca14891985
GuanJunMa <maguanjun@lanlingdai.net>
2018-11-12 23:56:17 +0800

    init project stage

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,35 @@
-require('normalize.css/normalize.css');
-require('styles/App.css');
+require('normalize.css/normalize.css')
+require('styles/App.css')
 
-import React from 'react';
+import React from 'react'
 
-let yeomanImage = require('../images/yeoman.png');
+let yeomanImage = require('../images/yeoman.png')
+
+let imageDatas = require('../data/imageDate.json')
+
+// 获取图片相关的数据
+imageDatas = (function genImageURL(imageDataArray) {
+  for (let i = 0; i < imageDataArray.length; i++) {
+    let singleImageData = imageDataArray[i]
+
+    singleImageData.imageURL = require(`../images/${singleImageData.fileName}`)
+
+    imageDataArray[i] = singleImageData
+  }
+  return imageDataArray
+})(imageDatas)
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
-    );
+      <section className="stage">
+        <section className="img-sec"></section>
+        <nav className="controller-nav"></nav>
+      </section>
+    )
   }
 }
 
-AppComponent.defaultProps = {
-};
+AppComponent.defaultProps = {}
 
-export default AppComponent;
+export default AppComponent
